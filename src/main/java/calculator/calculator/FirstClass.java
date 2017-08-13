c773c38f48a19ed98fb1cdfbb1ea0f3bb450c09c
“AdamK2 <ad200@gazeta.pl>
2017-08-13 12:37:59 +0100

    first commit

---
+++ src/main/java/calculator/calculator/FirstClass.java
@@ -0,0 +1,26 @@
+package calculator.calculator;
+
+class SimpleCalculator
+{
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
+
+class Application
+{
+    public static void main(String[] args) {
+
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int result = simpleCalculator.addAToB(5, 5);
+
+        System.out.println(result);
+
+    }
+}
+
